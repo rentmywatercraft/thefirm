79898d527d44fcd60e92ebd03cdb280aa366ae54
Jeff Hantke <jeff.hantke@gmail.com>
2021-03-22 10:33:51 -0600

    Minor UI fixes

--- TheFirmUI/src/app/new-employee/new-employee.component.ts
+++ TheFirmUI/src/app/new-employee/new-employee.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Console } from 'console';
 import { EmployeeModel } from 'src/models/employeeModel';
 import { JobTitleModel } from 'src/models/jobTitleModel';
 import { TheFirmService } from '../services/the-firm.service';
